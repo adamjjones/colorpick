c55bcf005cb4fd95e78c18c1411c4055a60792a2
Adam Jones <adam@linuxtampa.com>
2019-08-27 20:15:20 -0400

    Added random color button

--- src/components/Colorpicker.js
+++ src/components/Colorpicker.js
@@ -1,43 +1,63 @@
 import React, { useState } from 'react'
 
-const Colorpicker = () => {
-  const [Hue, setHue] = useState(0)
-  const [Saturation, setSaturation] = useState(0)
-  const [Lightness, setLightness] = useState(0)
-  const [Color, setColor] = useState(0)
+// function randomColor() {
+//   let random = Math.ceil(Math.random() * 255)
+// }
+
+componentDidMount() {
+  const Colorpicker = () => {
+    const [Hue, setHue] = useState(0)
+    const [Saturation, setSaturation] = useState(0)
+    const [Lightness, setLightness] = useState(0)
+    const [Color, setColor] = useState(0)
+  
+    const setRandomColor = () => {
+      setHue(Math.ceil(Math.random() * 360))
+      setSaturation(Math.ceil(Math.random() * 100))
+      setLightness(Math.ceil(Math.random() * 100))
+    }
+  
+}
+
+
+
   return (
-    <div className="slider">
-      <input
-        max="255"
-        type="range"
-        onChange={hue => {
-          setHue(hue.target.value)
-        }}
-      />
-      <p>{'Hue: ' + `${Hue}`}</p>
+    <div id="container">
+      <div className="slider">
+        <button onClick={setRandomColor}>Random color</button>
+        <input
+          max="255"
+          type="range"
+          onChange={hue => {
+            setHue(hue.target.value)
+          }}
+        />
+        <p>{'Hue: ' + `${Hue}`}</p>
 
-      <input
-        type="range"
-        onChange={saturation => {
-          setSaturation(saturation.target.value)
-        }}
-      />
-      <p>{'Saturation: ' + `${Saturation}`}</p>
+        <input
+          type="range"
+          onChange={saturation => {
+            setSaturation(saturation.target.value)
+          }}
+        />
+        <p>{'Saturation: ' + `${Saturation}`}</p>
 
-      <input
-        type="range"
-        onChange={lightness => {
-          setLightness(lightness.target.value)
-        }}
-      />
-      <p>{'Lightness: ' + `${Lightness}`}</p>
+        <input
+          type="range"
+          onChange={lightness => {
+            setLightness(lightness.target.value)
+          }}
+        />
+        <p>{'Lightness: ' + `${Lightness}`}</p>
 
-      <div
-        className="displayColor"
-        style={{
-          backgroundColor: `Hsl(${Hue}, ${Saturation}%, ${Lightness}%)`
-        }}
-      ></div>
+        <div
+          onLoad={setRandomColor}
+          className="displayColor"
+          style={{
+            backgroundColor: `Hsl(${Hue}, ${Saturation}%, ${Lightness}%)`
+          }}
+        ></div>
+      </div>
     </div>
   )
 }
