b87384cb6b1d7d08f23d7b76c6b6e5d8b7ee6f18
Adam Jones <adam@linuxtampa.com>
2019-08-27 21:54:54 -0400

    Added Alpha and CSS to random button

--- src/components/Colorpicker.js
+++ src/components/Colorpicker.js
@@ -1,32 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // function randomColor() {
 //   let random = Math.ceil(Math.random() * 255)
 // }
+const Colorpicker = () => {
+  const [Hue, setHue] = useState(0)
+  const [Saturation, setSaturation] = useState(0)
+  const [Lightness, setLightness] = useState(50)
+  const [Alpha, setAlpha] = useState(50)
 
-componentDidMount() {
-  const Colorpicker = () => {
-    const [Hue, setHue] = useState(0)
-    const [Saturation, setSaturation] = useState(0)
-    const [Lightness, setLightness] = useState(0)
-    const [Color, setColor] = useState(0)
-  
-    const setRandomColor = () => {
-      setHue(Math.ceil(Math.random() * 360))
-      setSaturation(Math.ceil(Math.random() * 100))
-      setLightness(Math.ceil(Math.random() * 100))
-    }
-  
-}
-
-
+  const setRandomColor = () => {
+    setHue(Math.ceil(Math.random() * 360))
+    setSaturation(Math.ceil(Math.random() * 100))
+    setLightness(Math.ceil(Math.random() * 100))
+  }
 
   return (
     <div id="container">
+      <h1>Color Picker with Hooks</h1>
       <div className="slider">
-        <button onClick={setRandomColor}>Random color</button>
         <input
-          max="255"
+          max="360"
           type="range"
           onChange={hue => {
             setHue(hue.target.value)
@@ -50,14 +44,24 @@ componentDidMount() {
         />
         <p>{'Lightness: ' + `${Lightness}`}</p>
 
+        <input
+          type="range"
+          onChange={alpha => {
+            setAlpha(alpha.target.value)
+          }}
+        />
+        <p>{'Alpha: ' + `${Alpha}`}</p>
+
         <div
           onLoad={setRandomColor}
           className="displayColor"
+          value="50"
           style={{
-            backgroundColor: `Hsl(${Hue}, ${Saturation}%, ${Lightness}%)`
+            backgroundColor: `Hsl(${Hue}, ${Saturation}%, ${Lightness}%, ${Alpha}%)`
           }}
         ></div>
       </div>
+      <button onClick={setRandomColor}>Random color</button>
     </div>
   )
 }
